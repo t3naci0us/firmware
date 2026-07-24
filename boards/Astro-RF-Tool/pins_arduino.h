5fa17da098cbbfeca9fc25147392ce07bfd66e6f
t3naci0us <nik.r.whale@gmail.com>
2026-07-24 22:55:29 +0100

    first lcd test

--- boards/Astro-RF-Tool/pins_arduino.h
+++ boards/Astro-RF-Tool/pins_arduino.h
@@ -4,124 +4,191 @@
 #include "soc/soc_caps.h"
 #include <stdint.h>
 
-static const uint8_t TX = 1;
-static const uint8_t RX = 2;
+/*
+ * Astro RF Tool
+ * ESP32-S3-N16R8
+ *
+ * TFT wiring:
+ * MOSI = GPIO11
+ * SCK  = GPIO12
+ * CS   = GPIO10
+ * DC   = GPIO9
+ * RST  = GPIO14
+ * BL   = permanently connected to 3.3V
+ */
 
-static const uint8_t SDA = 47;
-static const uint8_t SCL = 48;
+/* ---------------------------------------------------------
+   Default serial / UART
+   --------------------------------------------------------- */
 
-// Modified elsewhere
-static const uint8_t SS = 3;
-static const uint8_t MOSI = 17;
-static const uint8_t MISO = 8;
-static const uint8_t SCK = 18;
+static const uint8_t TX = 18;
+static const uint8_t RX = 17;
 
-#define SERIAL_RX 2
-#define SERIAL_TX 1
+#define SERIAL_RX 17
+#define SERIAL_TX 18
+
 #define BAD_RX SERIAL_RX
 #define BAD_TX SERIAL_TX
-#define GPS_SERIAL_TX SERIAL_TX
-#define GPS_SERIAL_RX SERIAL_RX
+
+#define GPS_SERIAL_RX 17
+#define GPS_SERIAL_TX 18
+
 #define USB_as_HID 1
+
+/* ---------------------------------------------------------
+   I2C — PN532
+   --------------------------------------------------------- */
+
+static const uint8_t SDA = 1;
+static const uint8_t SCL = 2;
+
+#define GROVE_SDA 1
+#define GROVE_SCL 2
+
+#define SYS_I2C_SDA 1
+#define SYS_I2C_SCL 2
+
+/* ---------------------------------------------------------
+   Five-way controls
+   --------------------------------------------------------- */
 
 #define BTN_ALIAS "\"OK\""
 #define HAS_5_BUTTONS
-#define SEL_BTN 0
-#define UP_BTN 41
+
+#define UP_BTN 39
 #define DW_BTN 40
-#define R_BTN 38
-#define L_BTN 39
+#define L_BTN 41
+#define R_BTN 42
+#define SEL_BTN 47
+
 #define BTN_ACT LOW
 
-#define RXLED 4
-#define TXLED 5
+/* ---------------------------------------------------------
+   IR
+   --------------------------------------------------------- */
+
+#define TXLED 8
+#define RXLED 21
+
 #define LED_ON HIGH
 #define LED_OFF LOW
 
+/* ---------------------------------------------------------
+   TFT display
+   --------------------------------------------------------- */
+
+#define HAS_SCREEN 1
+
+/*
+ * ROTATION may need changing after the first boot:
+ * 0 = portrait
+ * 1 = landscape
+ * 2 = inverted portrait
+ * 3 = inverted landscape
+ */
+#define ROTATION 1
+
+#define USER_SETUP_LOADED 1
+
+#define ST7735_DRIVER 1
+
+/*
+ * Try BLACKTAB first.
+ *
+ * If the image is displaced, corrupted or has incorrect
+ * colours, change this to one of:
+ *
+ * ST7735_REDTAB
+ * ST7735_GREENTAB
+ * ST7735_GREENTAB2
+ * ST7735_GREENTAB3
+ */
+#define ST7735_BLACKTAB 1
+
+#define TFT_WIDTH 128
+#define TFT_HEIGHT 160
+
+#define TFT_RGB_ORDER TFT_BGR
+
+#define TFT_MOSI 11
+#define TFT_SCLK 12
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_RST 14
+
+/*
+ * The backlight is connected directly to 3.3V,
+ * so no TFT_BL pin is defined.
+ */
+
+#define TOUCH_CS -1
+
+#define SMOOTH_FONT 1
+
+/*
+ * ST7735 displays are normally happier at 20–27 MHz.
+ */
+#define SPI_FREQUENCY 20000000
+#define SPI_READ_FREQUENCY 10000000
+#define SPI_TOUCH_FREQUENCY 2500000
+
+/* ---------------------------------------------------------
+   MicroSD
+   --------------------------------------------------------- */
+
+#define SDCARD_CS 4
+#define SDCARD_SCK 12
+#define SDCARD_MOSI 11
+#define SDCARD_MISO 13
+
+/* ---------------------------------------------------------
+   Shared RF SPI bus
+   --------------------------------------------------------- */
+
+#define SPI_SCK_PIN 12
+#define SPI_MOSI_PIN 11
+#define SPI_MISO_PIN 13
+#define SPI_SS_PIN 5
+
+/* ---------------------------------------------------------
+   CC1101
+   --------------------------------------------------------- */
+
 #define USE_CC1101_VIA_SPI
-#define CC1101_GDO0_PIN 9
-#define CC1101_GDO2_PIN 10
-#define CC1101_SS_PIN 46
+
+#define CC1101_SS_PIN 5
+#define CC1101_GDO0_PIN 6
+#define CC1101_GDO2_PIN 7
+
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
+/* ---------------------------------------------------------
+   NRF24L01
+   --------------------------------------------------------- */
+
 #define USE_NRF24_VIA_SPI
-#define NRF24_CE_PIN 21
-#define NRF24_SS_PIN 14
+
+#define NRF24_SS_PIN 15
+#define NRF24_CE_PIN 16
+
 #define NRF24_MOSI_PIN SPI_MOSI_PIN
 #define NRF24_SCK_PIN SPI_SCK_PIN
 #define NRF24_MISO_PIN SPI_MISO_PIN
+
+/* ---------------------------------------------------------
+   Buzzer
+   --------------------------------------------------------- */
+
+#define BUZZ_PIN 38
+
+/* ---------------------------------------------------------
+   Bruce frequency presets
+   --------------------------------------------------------- */
 
 #define FP 1
 #define FM 2
 #define FG 3
 
-#define HAS_SCREEN 1
-#define ROTATION 1
-#define MINBRIGHT (uint8_t)1
-
-#define USER_SETUP_LOADED 1
-#define ST7789_DRIVER 1
-#define TFT_RGB_ORDER 0
-#define TFT_WIDTH 170
-#define TFT_HEIGHT 320
-#define TFT_BACKLIGHT_ON 1
-#define TFT_BL 6
-#define TFT_RST 16
-#define TFT_DC 15
-#define TFT_MISO 8
-#define TFT_MOSI 17
-#define TFT_SCLK 18
-#define TFT_CS 7
-#define TOUCH_CS -1 // SDCARD_CS to make sure SDCard works
-#define SMOOTH_FONT 1
-#define SPI_FREQUENCY 20000000
-#define SPI_READ_FREQUENCY 20000000
-#define SPI_TOUCH_FREQUENCY 2500000
-
-#define SDCARD_CS 3
-#define SDCARD_SCK 18
-#define SDCARD_MISO 8
-#define SDCARD_MOSI 17
-
-#define GROVE_SDA 47
-#define GROVE_SCL 48
-#define SYS_I2C_SDA 47
-#define SYS_I2C_SCL 48
-
-#define SPI_SCK_PIN 13
-#define SPI_MOSI_PIN 12
-#define SPI_MISO_PIN 11
-#define SPI_SS_PIN 43
-
-// RGB LED
-
-#define HAS_RGB_LED 1
-#define RGB_LED 45
-#define LED_TYPE WS2812B
-#define LED_ORDER GRB
-#define LED_TYPE_IS_RGBW 0
-#define LED_COUNT 16
-
-#define LED_COLOR_STEP 15
-
-#define XPOWERS_CHIP_BQ25896
-
-// USE BOOST ENABLE PMIC 5V OUTPUT
-#define USE_BOOST
-
-// Mic#
-#define PIN_CLK 1
-#define PIN_DATA 10
-#define PIN_WS 2
-
-// IO EXPANDER
-#define USE_IO_EXPANDER
-#define IO_EXPANDER_AW9523
-#define IO_EXP_GPS 13
-#define IO_EXP_MIC 4
-#define IO_EXP_VIBRO 2
-#define IO_EXP_CC_RX 7
-#define IO_EXP_CC_TX 12
 #endif /* Pins_Arduino_h */
